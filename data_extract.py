5ab4e3171a6019c55448747d4359aea90bb694bf
Victor Breia <victordaschagas@outlook.com>
2024-04-22 10:25:02 -0300

    run to data_extract.py add lake files in csv

--- data_extract.py
+++ data_extract.py
@@ -20,7 +20,7 @@ usuarios = cursor.fetchall()
 with open("lake/usuarios.csv", "w") as arquivo:
     arquivo.write("id,nome,email,cpf,telefone,ano_nascimento,mes_nascimento,dia_nascimento,e_instrutor\n")
     for usuario in usuarios:
-        usuario = str(usuario).replace("(", "").replace(")", "").replace("datetime.date", "").replace("'", "")
+        usuario = str(usuario).replace("(", "").replace(")", "").replace("datetime.date", "").replace("'", "")æ
         arquivo.write(usuario + "\n")
 
 # Extrair dados da tabela cursos
